6b1f66a486a572a74ed07697c1385c6645065959
Rafael Herbert <rafaelherbert.dev@gmail.com>
2021-06-27 23:26:54 -0300

    Initial setup commit.

---
+++ src/pagamentos/Boleto.java
@@ -0,0 +1,9 @@
+package pagamentos;
+
+// Um boleto contém código do boleto, data, e valor pago.
+
+public class Boleto {
+	String codBoleto;
+	String data;
+	double valor;
+}
---
+++ src/pagamentos/Fatura.java
@@ -0,0 +1,5 @@
+package pagamentos;
+
+public class Fatura {
+
+}
